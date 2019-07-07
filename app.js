7bf125956b4e7b575fa4b42f50ac5880f6eed942
Sebastian Salazar <sebastian@lacaja.click>
2019-07-07 08:44:17 -0500

    Single item no errors working

--- app.js
+++ app.js
@@ -12,6 +12,30 @@ app.get('/api/v1/items', (req, res) => {
   })
 });
 
+// get single item
+
+app.get('/api/v1/items/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    let response = {
+        success: 'false',
+        message: 'item does not exist',
+    };
+    for (let i = 0; i < db.length; i++ ) {
+        if (db[i].id === id) {
+            response = {
+                success: 'true',
+                message: 'item retrieved successfully',
+                item: db[i],
+            }
+        }
+    };
+    if (response.success) {
+        return res.status(200).send(response);
+    } else {
+        return res.status(404).send(response);
+    };
+});
+
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
