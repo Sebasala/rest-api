b261a7fc6d3d191e7fb6272bb19b8829d3985bfb
Sebastian Salazar <sebastian@lacaja.click>
2019-07-07 07:21:00 -0500

    Database

---
+++ db/db.js
@@ -0,0 +1,12 @@
+const items = [
+    {
+        id: 1,
+        title: "Item 1",
+        description: "Description 1"
+    },
+    {
+        id: 2,
+        title: "Item 2",
+        description: "Description 2"
+    }
+]
